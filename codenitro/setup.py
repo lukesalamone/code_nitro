e02072b086a4f1ea0bb677e14d3cd8c6d9e4e966
lukesalamone <lukesalamone94@gmail.com>
2024-12-31 13:42:31 -0600

    increment version

--- codenitro/setup.py
+++ codenitro/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='codenitro',
-    version='1.0.1',
+    version='1.0.2',
     packages=['codenitro'],
     install_requires=[
         'Pillow>=10.0.0',
