822d54c5a1417a625932312b6f3c89b83ad8e8ec
lukesalamone <lukesalamone94@gmail.com>
2025-01-02 20:26:41 -0600

    enable programmatic import

--- codenitro/config.py
+++ codenitro/config.py
@@ -1,6 +1,60 @@
+from typing import Union
 from argparse import Namespace
 import json
 import os
+
+_SYSTEM_CONFIG_DIR = '~/.config/codenitro'
+_DEFAULT_CONFIG = {
+    # use default themes or add one to ~/.config/codenitro/themes.json
+    'theme':'desert',
+
+    # padding around code portion of image
+    'image_pad': 100
+}
+
+_DEFAULT_THEMES = {
+    'desert':{
+        # text styles can be found on https://pygments.org/styles
+        'text_style':'coffee',
+
+        # path to background image
+        'image_path':'images/desert.png',
+
+        # valid values are "gradient" or "none"
+        'background':'image'
+    },
+    'blue': {
+        # linear gradient from top left to bottom right
+        'background': 'gradient',
+
+        'text_style':'monokai',
+
+        # hex color format of top-left corner of gradient
+        'gradient_start': '#09afd4',
+
+        # hex color format of bottom-right corner of gradient
+        'gradient_end': '#341a6e'
+    },
+    'moon':{
+        'background':'image',
+        'text_style':'dracula',
+        'image_path':'images/moon.png',
+    },
+    'forest':{
+        'background':'image',
+        'text_style':'gruvbox-dark',
+        'image_path':'images/forest.png',
+    },
+    'cool':{
+        'background': 'gradient',
+        'text_style':'monokai',
+        'gradient_start': '#1faf98',
+        'gradient_end': '#dab4ff'
+    }
+}
+
+class BlankNamespace:
+    pass
 
 class Config:
     '''
@@ -13,75 +67,30 @@ class Config:
     be created on first run.
     '''
 
-    _SYSTEM_CONFIG_DIR = '~/.config/codenitro'
-    _DEFAULT_CONFIG = {
-        # use default themes or add one to ~/.config/codenitro/themes.json
-        'theme':'desert',
-
-        # padding around code portion of image
-        'image_pad': 100
-    }
-
-    _DEFAULT_THEMES = {
-        'desert':{
-            # text styles can be found on https://pygments.org/styles
-            'text_style':'coffee',
-
-            # path to background image
-            'image_path':'images/desert.png',
-
-            # valid values are "gradient" or "none"
-            'background':'image'
-        },
-        'blue': {
-            # linear gradient from top left to bottom right
-            'background': 'gradient',
-
-            'text_style':'monokai',
-
-            # hex color format of top-left corner of gradient
-            'gradient_start': '#09afd4',
-
-            # hex color format of bottom-right corner of gradient
-            'gradient_end': '#341a6e'
-        },
-        'moon':{
-            'background':'image',
-            'text_style':'dracula',
-            'image_path':'images/moon.png',
-        },
-        'cool':{
-            'background': 'gradient',
-            'text_style':'monokai',
-            'gradient_start': '#1faf98',
-            'gradient_end': '#dab4ff'
-        }
-    }
-
-    def __init__(self, command_line_args: Namespace):
-        self._command_line_args = command_line_args
+    def __init__(self, command_line_args: Union[Namespace, None] = None):
+        self._command_line_args = command_line_args if command_line_args else BlankNamespace()
         self._system_config = self._load_system_config()
         self._themes = self._load_themes()
         self._reconcile_configs()
 
     def _load_system_config(self):
-        system_config_dir = os.path.expanduser(self._SYSTEM_CONFIG_DIR)
+        system_config_dir = os.path.expanduser(_SYSTEM_CONFIG_DIR)
         system_config_path = os.path.join(system_config_dir, 'settings.json')
         os.makedirs(system_config_dir, exist_ok=True)
 
         if not os.path.exists(system_config_path):
             with open(system_config_path, 'w') as f:
-                json.dump(self._DEFAULT_CONFIG, f, indent=4)
+                json.dump(_DEFAULT_CONFIG, f, indent=4)
         with open(system_config_path) as f:
             return json.load(f)
 
     def _load_themes(self):
-        system_config_dir = os.path.expanduser(self._SYSTEM_CONFIG_DIR)
+        system_config_dir = os.path.expanduser(_SYSTEM_CONFIG_DIR)
         system_themes_path = os.path.join(system_config_dir, 'themes.json')
 
         if not os.path.exists(system_themes_path):
             with open(system_themes_path, 'w') as f:
-                json.dump(self._DEFAULT_THEMES, f, indent=4)
+                json.dump(_DEFAULT_THEMES, f, indent=4)
         with open(system_themes_path) as f:
             return json.load(f)
 
@@ -94,8 +103,7 @@ class Config:
             elif name in self._system_config:
                 return self._system_config[name]
             else:
-                return self._DEFAULT_CONFIG.get(name, None)
-
+                return _DEFAULT_CONFIG.get(name, None)
 
         def apply_theme_conf():
             if self.theme not in self._themes:
@@ -113,3 +121,19 @@ class Config:
         self.gradient_start = reconcile_single('gradient_start')
         self.gradient_end = reconcile_single('gradient_end')
         self.image_pad = reconcile_single('image_pad')
+
+    def get_theme_property(self, theme_name, val, key):
+        if key == 'text_style':
+            return self.text_style if val == '' else val
+        elif key == 'image_path':
+            return self.image_path if val == '' else val
+        elif key == 'background':
+            return self.image_path if val == '' else val
+        elif key == 'gradient_start':
+            return self.image_path if val == '' else val
+        elif key == 'gradient_end':
+            return self.image_path if val == '' else val
+        elif key == 'image_pad':
+            return self.image_pad if val == -1 else val
+        else:
+            raise ValueError(f'Unknown key `{key}`')
--- codenitro/nitro.py
+++ codenitro/nitro.py
@@ -10,13 +10,19 @@ import io
 
 from config import Config
 
-def add_shadow_and_gradient(image_data, config, use_gradient=False):
+def add_shadow_and_gradient(
+        image_data,
+        background=None,
+        gradient_start='#fff',
+        gradient_end='#000',
+        image_path='',
+        image_pad=0):
     def add_gradient(size):
         gradient = Image.new('RGBA', size, color=0)
         draw = ImageDraw.Draw(gradient)
 
-        start = ImageColor.getcolor(config.gradient_start, "RGB")
-        end = ImageColor.getcolor(config.gradient_end, "RGB")
+        start: tuple[int, ...] = ImageColor.getcolor(gradient_start, "RGB")
+        end: tuple[int, ...] = ImageColor.getcolor(gradient_end, "RGB")
         deltas = [(b - a) / gradient.width / 2 for a, b in zip(start, end)]
         for i, color in enumerate(range(gradient.width * 2)):
             color = [round(s + d * i) for s,d in zip(start, deltas)]
@@ -24,10 +30,10 @@ def add_shadow_and_gradient(image_data, config, use_gradient=False):
         return gradient
     def add_image(size):
         canvas = Image.new('RGBA', size, (255, 255, 255, 0))
-        additional_image = Image.open(config.image_path).convert("RGBA")
-        x_offset = (size[0] - additional_image.width) // 2
-        y_offset = (size[1] - additional_image.height) // 2
-        canvas.paste(additional_image, (x_offset, y_offset), additional_image)
+        bg_image = Image.open(image_path).convert("RGBA")
+        x_offset = (size[0] - bg_image.width) // 2
+        y_offset = (size[1] - bg_image.height) // 2
+        canvas.paste(bg_image, (x_offset, y_offset), bg_image)
         return canvas
     def make_shadow(code_dims, img_dims, offset_dims):
         shadow = Image.new('RGBA', code_dims, (20, 20, 20, 255))
@@ -37,11 +43,11 @@ def add_shadow_and_gradient(image_data, config, use_gradient=False):
         return shadow_holder
     image = Image.open(io.BytesIO(image_data))
     image = ImageEnhance.Color(image).enhance(2)
-    pad_x, pad_y = config.image_pad, config.image_pad
+    pad_x, pad_y = image_pad, image_pad
     offset_x, offset_y = 8, 8
     width, height = image.size
 
-    if config.background == 'gradient':
+    if background == 'gradient':
         bottom_canvas = add_gradient((width+pad_x, height+pad_y))
     else:
         bottom_canvas = add_image((width+pad_x, height+pad_y))
@@ -55,7 +61,7 @@ def add_shadow_and_gradient(image_data, config, use_gradient=False):
     bottom_canvas.paste(image, (pad_x//2, pad_y//2))
     return bottom_canvas
 
-def load_from_github(url):
+def load_from_github(url) -> tuple[str, str]:
     def convert_url(url):
         return '/'.join(url.split('/blob/')).replace('github.com/', 'raw.githubusercontent.com/')
 
@@ -69,44 +75,65 @@ def load_from_github(url):
         print(f'failed to download from url {url}')
         return '', ''
 
-def load_from_file(fname):
+def load_from_file(fname) -> tuple[str, str]:
     with open(fname) as f:
         text = f.read()
     return text, fname
 
-def get_range(lines_str):
+def get_range(lines_str) -> tuple[int, int]:
     scope = lines_str.split('-')
     assert len(scope) == 2, 'incorrect format for lines arg'
     start, end = [int(x) for x in scope]
     return start, end
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', type=str, help='the file to use')
-    parser.add_argument('--lines', type=str, help='range of lines to print (defaults to all)')
-    parser.add_argument('--theme', type=str, help='theme to use')
-    parser.add_argument('--text_style', type=str, help='text color scheme')
-    parser.add_argument('--image_pad', type=int, help='padding around code in pixels')
-    parser.add_argument('--background', type=str, help='gradient / none')
-    args = parser.parse_args()
+def code_to_image(
+        file_input=None,
+        text_input=None,
+        link_input=None,
+        line_range=None,
+        theme='desert',
+        text_style='',
+        background='',
+        gradient_start='',
+        gradient_end='',
+        image_path='',
+        image_pad=-1,
+        outpath=None,
+        save=True):
+    if len([x for x in [file_input, text_input, link_input] if x]) != 1:
+        raise ValueError(
+            'Exactly one of file_input, text_input, or link_input must be specified.'
+        )
 
-    config = Config(command_line_args=args)
+    # user-specified theme will override sub-properties
+    if theme:
+        config = Config()
+        text_style = config.get_theme_property(theme, text_style, 'text_style')
+        background = config.get_theme_property(theme, background, 'background')
+        gradient_start = config.get_theme_property(theme, gradient_start, 'gradient_start')
+        gradient_end = config.get_theme_property(theme, gradient_end, 'gradient_end')
+        image_path = config.get_theme_property(theme, image_path, 'image_path')
+        image_pad = config.get_theme_property(theme, image_pad, 'image_pad')
 
-    if 'http://' in args.input or 'https://' in args.input:
-        text, fname = load_from_github(args.input)
+    fname = 'default_filename.py'
+    if text_input:
+        text = text_input
+        lines = text_input.split('\n')
     else:
-        text, fname = load_from_file(args.input)
-
+        if link_input != None:
+            text, fname = load_from_github(link_input)
+        else:
+            text, fname = load_from_file(file_input)
+        lines = text.split('\n')
     start = 1
-    lines = text.split('\n')
     line_count = len(lines)
-    if args.lines:
-        start, end = get_range(args.lines)
+    if line_range:
+        start, end = get_range(line_range)
         print(f'including only lines in range {start} - {end}')
         text = '\n'.join(lines[start-1:end])
 
     line_number_chars = int(math.log10(line_count) + 1)
-    style = get_style_by_name(config.text_style)
+    style = get_style_by_name(text_style)
     formatter = ImageFormatter(
         style=style,
         line_number_chars=line_number_chars,
@@ -119,15 +146,68 @@ def main():
     lexer = get_lexer_for_filename(fname)
     result = pygments.highlight(text, lexer, formatter)
     name_part = '.'.join(fname.split('.')[:-1])
-    outname = f'{name_part}.png'
-    if not config.background == 'none':
-        result = add_shadow_and_gradient(result, config)
-    with open(outname, 'wb') as f:
+
+    if not outpath:
+        outpath = f'{name_part}.png'
+
+    if background != 'none':
+        result = add_shadow_and_gradient(
+            image_data=result,
+            background=background,
+            gradient_start=gradient_start,
+            gradient_end=gradient_end,
+            image_path=image_path,
+            image_pad=image_pad)
+
+    if not save:
+        return result
+
+    with open(outpath, 'wb') as f:
         if isinstance(result, Image.Image):
             result.save(f)
         else:
             f.write(result)
-        print(f'saved image to {outname}')
+        print(f'saved image to {outpath}')
+
+    return result
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=str, help='the file to use')
+    parser.add_argument('--lines', type=str, help='range of lines to print (defaults to all)')
+    parser.add_argument('--theme', type=str, help='theme to use')
+    parser.add_argument('--text_style', type=str, help='text color scheme')
+    parser.add_argument('--image_pad', type=int, help='padding around code in pixels')
+    parser.add_argument('--background', type=str, help='gradient / none')
+    args = parser.parse_args()
+
+    config = Config(command_line_args=args)
+    text_input, link_input, file_input = None, None, None
+
+    if 'http://' in args.input or 'https://' in args.input:
+        link_input = args.input
+    else:
+        file_input = args.input
+
+    code_to_image(
+        file_input=file_input,
+        text_input=text_input,
+        link_input=link_input,
+        line_range=args.lines,
+        theme=config.theme,
+        text_style=config.text_style,
+        background=config.background,
+        gradient_start=config.gradient_start,
+        gradient_end=config.gradient_end,
+        image_path=config.image_path,
+        image_pad=config.image_pad,
+        outpath=None,
+        save=True
+    )
 
 if __name__ == '__main__':
     main()
+
+__all__ = [
+    'code_to_image'
+]
